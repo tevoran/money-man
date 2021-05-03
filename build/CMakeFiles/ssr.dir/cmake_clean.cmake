cdc9a3322b160e64a2012ec08500b2db8ded202f
tevoran <j.pfeiffer93@gmx.de>
2021-05-03 16:44:25 +0200

    buildchain setup

---
+++ build/CMakeFiles/ssr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ssr.dir/src/main.cpp.o"
+  "ssr"
+  "ssr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ssr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
