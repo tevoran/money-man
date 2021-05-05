774af654cec5fad14a76766761a5d11c7121a8db
tevoran <j.pfeiffer93@gmx.de>
2021-05-05 20:59:49 +0200

    simple money sprite created and included

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main()
 
 	mm::object player(&game, "../assets/player.png", RES_X/2, 0, 43, 127);
 	mm::object floor(&game, "../assets/ground_tile.png", 0, RES_Y-FLOOR_OFFSET, 64, 64);
+	mm::object dollar(&game, "../assets/dollar.png", 500, 150, 64, 64);
 
 	//main loop
 	bool quit=false;
@@ -31,7 +32,9 @@ int main()
 		player.physics_update(frametime.frametime_sec());
 
 		//render
+		dollar.render();
 		player.render();
+
 		for(int i=0; i<((RES_X/64)+1); i++)
 		{
 			floor.render(i*64,RES_Y-FLOOR_OFFSET);
